42a451aa1046c959225dc62f8078821c4fc1703e
Ever Ruiz Diaz <ruizdiaz.oe@gmail.com>
2023-01-31 08:09:05 +0100

    Minor change

--- README.md
+++
@@ -1,3 +0,0 @@
-# Bots
-
-Bot army
--- src/bots/xtx/docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Docker
-docker image build -t redchiche .           # Build with detach mode in .
-echo 'IMAGE OF DOCKER BUILDED'              # INFO
-docker run -td redchiche                    # RUN in background
-echo 'READY AND RUNNING IN BACKGROUND'
